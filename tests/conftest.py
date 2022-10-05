c8e7bd799ac62280b3fc791720df018d08ee6dbb
yavv951 <vadimv95189@gmail.com>
2022-10-05 11:58:48 +0300

    add test mobile local server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,19 @@
 import os
+
+import allure
 import pytest
-from dotenv import load_dotenv
-from appium import webdriver
-from datetime import date
+from _pytest.nodes import Item
+from _pytest.runner import CallInfo
+from allure_commons._allure import StepContext
+from selene.support._logging import wait_with
 
 from selene.support.shared import browser
+from appium import webdriver
+from dotenv import load_dotenv
+from datetime import date
+
+import config
+from utils import utils
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -14,8 +23,50 @@ def load_env():
     """
     load_dotenv()
 
+
+@pytest.fixture(scope='function', autouse=True)
+def driver_management(request):
+    browser.config.timeout = config.settings.timeout
+    browser.config._wait_decorator = wait_with(
+        context=StepContext
+    )
+    with allure.step('set up app session'):
+        browser.config.driver = webdriver.Remote(
+            config.settings.remote_url, options=config.settings.driver_options
+        )
+
+    yield
+
+    # given we want to save disk space
+    # then we store screenshots and xml dumps only for failed tests
+    if config.settings.run_on_browserstack and request.node.result_of_call.failed:
+        '''
+        request.node is an "item" because we use the default "function" scope
+        '''
+        utils.allure.attach.screenshot(name='Last screenshot')
+        utils.allure.attach.screen_xml_dump()
+
+    session_id = browser.driver.session_id
+
+    allure.step('close app session')(browser.quit)()
+
+    if config.settings.run_on_browserstack:
+        utils.allure.attach.video_from_browserstack(session_id)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item: Item, call: CallInfo):  # noqa
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    result_of_ = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+    setattr(item, 'result_of_' + result_of_.when, result_of_)
+
+
 @pytest.fixture(scope='function')
-def app_android():
+def app_android_brs():
     """
     Create driver
     """
@@ -41,5 +92,5 @@ def app_android():
         desired_capabilities=desired_cap
     )
     browser.config.timeout = 4
-    yield app_android
+    yield app_android_brs
     browser.quit()
--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -1,15 +1,17 @@
 import allure
 from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import have
+from selene import have, be
 from selene.support.shared import browser
 
-from util.attachment import add_video
+from atach.attachment import add_video
+from utils.model import app
 
 
 @allure.tag('mobile')
 @allure.title('Test search')
-def test_wiki_browserstack(app_android):
+def test_wiki_browserstack():
+    app.given_opened()
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('BrowserStack')
@@ -21,7 +23,8 @@ def test_wiki_browserstack(app_android):
 
 @allure.tag('mobile')
 @allure.title('Test search')
-def test_wiki_sqa(app_android):
+def test_wiki_not_found_text():
+    app.given_opened()
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Software quality assurance")
